0b23e64f2fefcc87abb4b5a7e22ee6caef0e0459
deven shona <Devenshona@gmail.com>
2026-07-12 14:53:06 -0500

    feature: add additional input handling and movement methods to FPSCoreCharacter

--- Source/FPSCore/Public/Character/FPSCoreCharacter.h
+++ Source/FPSCore/Public/Character/FPSCoreCharacter.h
@@ -4,11 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "AbilitySystemInterface.h"
+#include "GameplayTagContainer.h"
 #include "GameFramework/Character.h"
 #include "FPSCoreCharacter.generated.h"
 
 class UFPSCoreAbilitySystemComponent;
 class UAbilitySystemComponent;
+class AFPSCorePlayerState;
+struct FInputActionValue;
 
 UCLASS()
 class FPSCORE_API AFPSCoreCharacter : public ACharacter, public IAbilitySystemInterface
@@ -28,12 +31,20 @@ protected:
 
 	virtual void PossessedBy(AController* NewController) override;
 	virtual void OnRep_PlayerState() override;
-	
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	virtual void UnPossessed() override;
 private:
+	void Input_Move(const FInputActionValue& Value);
+	void Input_Look(const FInputActionValue& Value);
+	void Input_Jump(const FInputActionValue& Value) {};
+	void Input_Crouch(const FInputActionValue& Value){};
+	
+	void Input_AbilityTagPressed(FGameplayTag InputTag);
+	void Input_AbilityTagReleased(FGameplayTag InputTag);
+	
 	void InitializeAbilitySystem();
 	
 public:
 	virtual void Tick(float DeltaTime) override;
 
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 };
