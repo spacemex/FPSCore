db6e58c22841b54b525c1703d857e2f80650a596
deven shona <Devenshona@gmail.com>
2026-07-12 14:53:19 -0500

    feature: add helper methods to retrieve player state and player ID in FPSCoreGameplayAbility

--- Source/FPSCore/GAS/FPSCoreGameplayAbility.cpp
+++ Source/FPSCore/GAS/FPSCoreGameplayAbility.cpp
@@ -1,1 +1,19 @@
 #include "FPSCoreGameplayAbility.h"
+
+#include "Character/FPSCorePlayerState.h"
+
+AFPSCorePlayerState* UFPSCoreGameplayAbility::GetFPSCorePlayerState() const
+{
+	return Cast<AFPSCorePlayerState>(GetOwningActorFromActorInfo());
+}
+
+int32 UFPSCoreGameplayAbility::GetOwningPlayerId() const
+{
+	const AFPSCorePlayerState* FPSPlayerState = GetFPSCorePlayerState();
+
+	if (!IsValid(FPSPlayerState))
+	{
+		return INDEX_NONE;
+	}
+	return FPSPlayerState->GetPlayerId();
+}
