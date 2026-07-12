51dda864ed04eb73007d89a1873d41482724eba5
deven shona <Devenshona@gmail.com>
2026-07-12 14:54:30 -0500

    feature: add AFPSCorePlayerController with input config support and input processing

---
+++ Source/FPSCore/Public/Character/FPSCorePlayerController.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "FPSCorePlayerController.generated.h"
+
+class UFPSCoreInputConfig;
+
+UCLASS()
+class FPSCORE_API AFPSCorePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void PostProcessInput(const float DeltaTime, const bool bGamePaused) override;
+	
+	UFUNCTION(BlueprintPure,Category="FPS Core|Input")
+	const UFPSCoreInputConfig* GetInputConfig() const {return InputConfig;}
+	
+protected:
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="FPS Core|Input")
+	TObjectPtr<UFPSCoreInputConfig> InputConfig;
+	
+private:
+	void ApplyInputMappings();
+};
