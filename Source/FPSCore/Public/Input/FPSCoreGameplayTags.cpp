0ad2854b47fee0155280bc9320b342414e9be966
deven shona <Devenshona@gmail.com>
2026-07-12 14:53:36 -0500

    feature: add gameplay tags for input actions in FPSCore

---
+++ Source/FPSCore/Public/Input/FPSCoreGameplayTags.cpp
@@ -0,0 +1,9 @@
+﻿#include "FPSCoreGameplayTags.h"
+
+namespace FPSCoreGameplayTags
+{
+	UE_DEFINE_GAMEPLAY_TAG(Input_Move,"Input.Ability.Movement.Move");
+	UE_DEFINE_GAMEPLAY_TAG(Input_Look,"Input.Ability.Movement.Look");
+	UE_DEFINE_GAMEPLAY_TAG(Input_Jump,"Input.Ability.Movement.Jump");
+	UE_DEFINE_GAMEPLAY_TAG(Input_Crouch,"Input.Ability.Movement.Crouch");
+}
