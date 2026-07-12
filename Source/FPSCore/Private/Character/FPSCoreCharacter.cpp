c5ca9af0e6863a4e8c563df0d0781a91d60757bd
deven shona <Devenshona@gmail.com>
2026-07-12 14:52:55 -0500

    feature: add input binding and movement controls to FPSCoreCharacter

--- Source/FPSCore/Private/Character/FPSCoreCharacter.cpp
+++ Source/FPSCore/Private/Character/FPSCoreCharacter.cpp
@@ -3,7 +3,12 @@
 
 #include "FPSCore/Public/Character/FPSCoreCharacter.h"
 
+#include "EnhancedInputComponent.h"
+#include "Character/FPSCorePlayerController.h"
 #include "FPSCore/Public/Character/FPSCorePlayerState.h"
+#include "Input/FPSCoreGameplayTags.h"
+#include "Input/FPSCoreInputBinding.h"
+#include "Input/FPSCoreInputConfig.h"
 
 
 AFPSCoreCharacter::AFPSCoreCharacter()
@@ -26,6 +31,43 @@ void AFPSCoreCharacter::Tick(float DeltaTime)
 void AFPSCoreCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	
+	UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+
+	if (!IsValid(EnhancedInputComponent))
+	{
+		UE_LOG(LogTemp,Error,TEXT("%s requires UEnhancedInputComponent"),*GetNameSafe(this));
+		return;
+	}
+	
+	const AFPSCorePlayerController* FPSController = Cast<AFPSCorePlayerController>(GetController());
+
+	if (!IsValid(FPSController))
+	{
+		UE_LOG(LogTemp,Error,TEXT("%s requires AFPSCorePlayerController"),*GetNameSafe(this));
+		return;
+	}
+	
+	const UFPSCoreInputConfig* InputConfig = FPSController->GetInputConfig();
+
+	if (!IsValid(InputConfig))
+	{
+		UE_LOG(LogTemp,Error,TEXT("%s does not have a valid Input Config"),*GetNameSafe(FPSController));
+		return;
+	}
+	
+	FPSCoreInput::BindNativeAction(EnhancedInputComponent,InputConfig,FPSCoreGameplayTags::Input_Look,
+		ETriggerEvent::Triggered,this,&ThisClass::Input_Look);
+	FPSCoreInput::BindNativeAction(EnhancedInputComponent,InputConfig,FPSCoreGameplayTags::Input_Move,
+		ETriggerEvent::Triggered,this,&ThisClass::Input_Move);
+	FPSCoreInput::BindNativeAction(EnhancedInputComponent,InputConfig,FPSCoreGameplayTags::Input_Crouch,
+		ETriggerEvent::Triggered,this,&ThisClass::Input_Crouch);
+	FPSCoreInput::BindNativeAction(EnhancedInputComponent,InputConfig,FPSCoreGameplayTags::Input_Jump,
+		ETriggerEvent::Triggered,this,&ThisClass::Input_Jump);
+	
+	FPSCoreInput::BindAbilityActions(EnhancedInputComponent,InputConfig,this,&ThisClass::Input_AbilityTagPressed,
+		&ThisClass::Input_AbilityTagReleased);
+	
 }
 
 void AFPSCoreCharacter::PossessedBy(AController* NewController)
@@ -45,7 +87,7 @@ void AFPSCoreCharacter::InitializeAbilitySystem()
 {
 	AFPSCorePlayerState* FPSPlayerState = GetPlayerState<AFPSCorePlayerState>();
 
-	if (IsValid(FPSPlayerState))
+	if (!IsValid(FPSPlayerState))
 	{
 		UE_LOG(LogTemp,Warning,TEXT("%s does not have FPSCorePlayerState."),*GetNameSafe(this));
 		return;
@@ -63,10 +105,66 @@ UFPSCoreAbilitySystemComponent* AFPSCoreCharacter::GetFPSCoreAbilitySystemCompon
 {
 	const AFPSCorePlayerState* FPSPlayerState = GetPlayerState<AFPSCorePlayerState>();
 
-	if (IsValid(FPSPlayerState))
+	if (!IsValid(FPSPlayerState))
 	{
 		return nullptr;
 	}
 	
 	return FPSPlayerState->GetFPSCoreAbilitySystemComponent();
 }
+
+void AFPSCoreCharacter::Input_Move(const FInputActionValue& Value)
+{
+	if (!IsValid(Controller))
+	{
+		return;
+	}
+	
+	const FVector2D MovementValue = Value.Get<FVector2D>();
+	const FRotator ControlRotation = Controller->GetControlRotation();
+	
+	const FRotator YawRotation(0.0,ControlRotation.Yaw,0.0);
+	
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	AddMovementInput(ForwardDirection,MovementValue.Y);
+	AddMovementInput(RightDirection,MovementValue.X);
+}
+
+void AFPSCoreCharacter::Input_Look(const FInputActionValue& Value)
+{
+	const FVector2D LookValue = Value.Get<FVector2D>();
+
+	AddControllerYawInput(LookValue.X);
+	AddControllerPitchInput(LookValue.Y);
+}
+
+void AFPSCoreCharacter::Input_AbilityTagPressed(FGameplayTag InputTag)
+{
+	UFPSCoreAbilitySystemComponent* ASC = GetFPSCoreAbilitySystemComponent();
+	if (IsValid(ASC) && InputTag.IsValid())
+	{
+		ASC->AbilityInputTagPressed(InputTag);
+	}
+}
+
+void AFPSCoreCharacter::Input_AbilityTagReleased(FGameplayTag InputTag)
+{
+	UFPSCoreAbilitySystemComponent* ASC = GetFPSCoreAbilitySystemComponent();
+	if (IsValid(ASC) && InputTag.IsValid())
+	{
+		ASC->AbilityInputTagReleased(InputTag);
+	}
+}
+
+void AFPSCoreCharacter::UnPossessed()
+{
+	if (UFPSCoreAbilitySystemComponent* ASC = GetFPSCoreAbilitySystemComponent())
+	{
+		ASC->ClearAbilityInput();
+	}
+	Super::UnPossessed();
+}
+
