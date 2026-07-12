56ee4bc1f031365a7f20a3e9f30e658bef76cba3
deven shona <Devenshona@gmail.com>
2026-07-12 14:53:49 -0500

    feature: add method to find native input actions by gameplay tag in FPSCoreInputConfig

---
+++ Source/FPSCore/Private/Input/FPSCoreInputAction.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Input/FPSCoreInputConfig.h"
+
+const UInputAction* UFPSCoreInputConfig::FindNativeInputActionForTag(const FGameplayTag& InputTag, bool bLogNotFound) const
+{
+	if (!InputTag.IsValid())
+	{
+		return nullptr;
+	}
+	
+	for (const FFPSCoreInputAction& InputAction : NativeInputActions)
+	{
+		if (IsValid(InputAction.InputAction) && InputAction.InputTag.MatchesTagExact(InputTag))
+		{
+			return InputAction.InputAction;
+		}
+	}
+
+	if (bLogNotFound)
+	{
+		UE_LOG(LogTemp,Error,TEXT("Could not find native input action for tag [%s] in Input Config [%s]"),*InputTag.ToString(),
+			*GetNameSafe(this));
+	}
+	return nullptr;
+}
