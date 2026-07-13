a4f38f7065f69750e4532b4e7841d1a312b8348a
deven shona <Devenshona@gmail.com>
2026-07-13 06:01:03 -0500

    feature: add UFPSCoreHealthComponent with health management, replication, and death state handling

---
+++ Source/FPSCore/Private/Components/FPSCoreHealthComponent.cpp
@@ -0,0 +1,191 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/FPSCoreHealthComponent.h"
+
+#include "FPSCore/GAS/FPSCoreAbilitySystemComponent.h"
+#include "FPSCore/GAS/Attributes/FPSCoreHealthSet.h"
+#include "Net/UnrealNetwork.h"
+
+
+UFPSCoreHealthComponent::UFPSCoreHealthComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	SetIsReplicatedByDefault(true);
+}
+
+void UFPSCoreHealthComponent::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	
+	DOREPLIFETIME(UFPSCoreHealthComponent,DeathState);
+}
+
+void UFPSCoreHealthComponent::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	UninitializeFromAbilitySystem();
+	Super::EndPlay(EndPlayReason);
+}
+
+void UFPSCoreHealthComponent::InitializeWithAbilitySystem(UFPSCoreAbilitySystemComponent* InAbilitySystemComponent, UFPSCoreHealthSet* InHealthSet)
+{
+	if (!IsValid(InAbilitySystemComponent) || !IsValid(InHealthSet))
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s cannot initialize without a valid AbilitySystemComponent and HealthSet"), *GetName());
+		return;
+	}
+
+	if (AbilitySystemComponent == InAbilitySystemComponent && HealthSet == InHealthSet)
+	{
+		return;
+	}
+	
+	UninitializeFromAbilitySystem();
+	
+	AbilitySystemComponent = InAbilitySystemComponent;
+	HealthSet = InHealthSet;
+	
+	HealthChangedDelegateHandle = AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(UFPSCoreHealthSet::GetHealthAttribute())
+	.AddUObject(this,&ThisClass::HandHealthChanged);
+	
+	MaxHealthChangedDelegateHandle = AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(UFPSCoreHealthSet::GetMaxHealthAttribute())
+	.AddUObject(this,&ThisClass::HandMaxHealthChanged);
+	
+	const float CurrentHealth = HealthSet->GetHealth();
+	const float CurrentMaxHealth = HealthSet->GetMaxHealth();
+	
+	OnMaxHealthChanged.Broadcast(this,CurrentMaxHealth,CurrentMaxHealth);
+	OnHealthChanged.Broadcast(this,CurrentHealth,CurrentHealth);
+
+	if (GetOwner()->HasAuthority() && CurrentHealth <= 0.0f)
+	{
+		StartDeath();
+	}
+}
+
+void UFPSCoreHealthComponent::UninitializeFromAbilitySystem()
+{
+	if (IsValid(AbilitySystemComponent))
+	{
+		if (HealthChangedDelegateHandle.IsValid())
+		{
+			AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(UFPSCoreHealthSet::GetHealthAttribute())
+			.Remove(HealthChangedDelegateHandle);
+		}
+		if (MaxHealthChangedDelegateHandle.IsValid())
+		{
+			AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(UFPSCoreHealthSet::GetMaxHealthAttribute())
+			.Remove(MaxHealthChangedDelegateHandle);
+		}
+	}
+	
+	HealthChangedDelegateHandle.Reset();
+	MaxHealthChangedDelegateHandle.Reset();
+	
+	AbilitySystemComponent = nullptr;
+	HealthSet = nullptr;
+}
+
+float UFPSCoreHealthComponent::GetHealth() const
+{
+	return IsValid(HealthSet) ? HealthSet->GetHealth() : 0.0f;
+}
+
+float UFPSCoreHealthComponent::GetMaxHealth() const
+{
+	return IsValid(HealthSet) ? HealthSet->GetMaxHealth() : 0.0f;
+}
+
+float UFPSCoreHealthComponent::GetHealthNormalized() const
+{
+	const float CurrentMaxHealth = GetMaxHealth();
+
+	if (CurrentMaxHealth <= 0.f)
+	{
+		return 0.0f;
+	}
+	
+	return FMath::Clamp(GetHealth() / CurrentMaxHealth, 0.0f, 1.0f);
+}
+
+void UFPSCoreHealthComponent::HandHealthChanged(const FOnAttributeChangeData& ChangeData)
+{
+	OnHealthChanged.Broadcast(this,ChangeData.OldValue,ChangeData.NewValue);
+
+	if (GetOwner()->HasAuthority() && ChangeData.OldValue > 0.f && ChangeData.NewValue <= 0.f)
+	{
+		StartDeath();
+	}
+}
+
+void UFPSCoreHealthComponent::HandMaxHealthChanged(const FOnAttributeChangeData& ChangeData)
+{
+	OnMaxHealthChanged.Broadcast(this, ChangeData.OldValue,ChangeData.NewValue);
+}
+
+void UFPSCoreHealthComponent::StartDeath()
+{
+	if (!GetOwner()->HasAuthority() || DeathState != EFPSCoreDeathState::NotDead)
+	{
+		return;
+	}
+	
+	SetDeathState(EFPSCoreDeathState::DeathStarted);
+}
+
+void UFPSCoreHealthComponent::FinishDeath()
+{
+	if (!GetOwner()->HasAuthority() || DeathState != EFPSCoreDeathState::DeathStarted)
+	{
+		return;
+	}
+	
+	SetDeathState(EFPSCoreDeathState::DeathFinished);
+}
+
+void UFPSCoreHealthComponent::SetDeathState(EFPSCoreDeathState NewDeathState)
+{
+	if (DeathState == NewDeathState)
+	{
+		return;
+	}
+	const EFPSCoreDeathState OldDeathState = DeathState;
+	
+	DeathState = NewDeathState;
+	
+	HandleDeathStateChanged(OldDeathState);
+}
+
+void UFPSCoreHealthComponent::OnRep_DeathState(EFPSCoreDeathState OldDeathState)
+{
+	HandleDeathStateChanged(OldDeathState);
+}
+
+void UFPSCoreHealthComponent::HandleDeathStateChanged(EFPSCoreDeathState OldDeathState)
+{
+	if (OldDeathState == EFPSCoreDeathState::NotDead && DeathState == EFPSCoreDeathState::DeathStarted)
+	{
+		OnDeathStarted.Broadcast(this);
+		return;
+	}
+	if (OldDeathState == EFPSCoreDeathState::DeathStarted && DeathState == EFPSCoreDeathState::DeathFinished)
+	{
+		OnDeathFinished.Broadcast(this);
+	}
+}
+
+void UFPSCoreHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+}
+
+
+void UFPSCoreHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                            FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+}
+
---
+++ Source/FPSCore/Public/Components/FPSCoreHealthComponent.h
@@ -0,0 +1,109 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "FPSCoreHealthComponent.generated.h"
+
+class UFPSCoreAbilitySystemComponent;
+class UFPSCoreHealthSet;
+struct FOnAttributeChangeData;
+
+UENUM(BlueprintType)
+enum class EFPSCoreDeathState : uint8
+{
+	NotDead,
+	DeathStarted,
+	DeathFinished,
+};
+
+class UFPSCoreHealthComponent;
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FFPSCoreHealthChangedSignature,UFPSCoreHealthComponent*, HealthComponent,
+	float, OldValue, float, NewValue);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FFPSCoreDeathEventSignature, UFPSCoreHealthComponent*, HealthComponent);
+
+
+UCLASS(BlueprintType,Blueprintable,ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class FPSCORE_API UFPSCoreHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UFPSCoreHealthComponent();
+
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+	
+	void InitializeWithAbilitySystem(UFPSCoreAbilitySystemComponent* InAbilitySystemComponent, UFPSCoreHealthSet* InHealthSet);
+	void UninitializeFromAbilitySystem();
+	
+	UFUNCTION(BlueprintPure,Category="FPS Core|Health")
+	UFPSCoreAbilitySystemComponent* GetAbilitySystemComponent() const
+	{
+		return AbilitySystemComponent;
+	}
+	
+	UFUNCTION(BlueprintPure,Category="FPS Core|Health")
+	UFPSCoreHealthSet* GetHealthSet() const
+	{
+		return HealthSet;
+	}
+	
+	UFUNCTION(BlueprintPure,Category="FPS Core|Health")
+	float GetHealth() const;
+	UFUNCTION(BlueprintPure,Category="FPS Core|Health")
+	float GetMaxHealth() const;
+	UFUNCTION(BlueprintPure,Category="FPS Core|Health")
+	float GetHealthNormalized() const;
+	UFUNCTION(BlueprintPure,Category="FPS Core|Healt")
+	EFPSCoreDeathState GetDeathState() const
+	{
+		return DeathState;
+	}
+	UFUNCTION(BlueprintPure,Category="FPS Core|Health")
+	bool IsDead() const
+	{
+		return DeathState != EFPSCoreDeathState::NotDead;
+	}
+	UFUNCTION(BlueprintCallable,Category="FPS Core|Health")
+	void FinishDeath();
+	
+	UPROPERTY(BlueprintAssignable,Category="FPS Core|Health")
+	FFPSCoreHealthChangedSignature OnHealthChanged;
+	UPROPERTY(BlueprintAssignable,Category="FPS Core|Health")
+	FFPSCoreHealthChangedSignature OnMaxHealthChanged;
+	UPROPERTY(BlueprintAssignable,Category="FPS Core|Health")
+	FFPSCoreDeathEventSignature OnDeathStarted;
+	UPROPERTY(BlueprintAssignable,Category="FPS Core|Health")
+	FFPSCoreDeathEventSignature OnDeathFinished;
+	
+private:
+	void HandHealthChanged(const FOnAttributeChangeData& ChangeData);
+	void HandMaxHealthChanged(const FOnAttributeChangeData& ChangeData);
+	void StartDeath();
+	void SetDeathState(EFPSCoreDeathState NewDeathState);
+	void HandleDeathStateChanged(EFPSCoreDeathState OldDeathState);
+	
+	UFUNCTION()
+	void OnRep_DeathState(EFPSCoreDeathState OldDeathState);
+	
+	UPROPERTY(Transient)
+	TObjectPtr<UFPSCoreAbilitySystemComponent>AbilitySystemComponent;
+
+	UPROPERTY(Transient)
+	TObjectPtr<UFPSCoreHealthSet> HealthSet;
+
+	UPROPERTY(ReplicatedUsing = OnRep_DeathState,Transient)EFPSCoreDeathState DeathState = EFPSCoreDeathState::NotDead;
+
+	FDelegateHandle HealthChangedDelegateHandle;
+	FDelegateHandle MaxHealthChangedDelegateHandle;
+	
+	
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+};
