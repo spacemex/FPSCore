c1b096ead921a9beaf1e176be9c2f86e92739319
deven shona <Devenshona@gmail.com>
2026-07-13 06:00:56 -0500

    feature: add UFPSCoreHealthSet with health and max health attributes, replication, and attribute change handling

---
+++ Source/FPSCore/GAS/Attributes/FPSCoreHealthSet.cpp
@@ -0,0 +1,69 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSCoreHealthSet.h"
+
+#include "GameplayEffectExtension.h"
+#include "Net/UnrealNetwork.h"
+
+
+UFPSCoreHealthSet::UFPSCoreHealthSet()
+{
+	InitMaxHealth(100.f);
+	InitHealth(100.f);
+}
+
+void UFPSCoreHealthSet::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	
+	DOREPLIFETIME_CONDITION_NOTIFY(UFPSCoreHealthSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UFPSCoreHealthSet, MaxHealth, COND_None, REPNOTIFY_Always);
+}
+
+void UFPSCoreHealthSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
+
+	if (Attribute == GetMaxHealthAttribute())
+	{
+		NewValue = FMath::Max(NewValue, 1.f);
+	}else if (Attribute == GetHealthAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxHealth());
+	}
+}
+
+void UFPSCoreHealthSet::PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue)
+{
+	Super::PostAttributeChange(Attribute, OldValue, NewValue);
+
+	if (Attribute == GetMaxHealthAttribute() && GetHealth() > NewValue)
+	{
+		SetHealth(NewValue);
+	}
+}
+
+void UFPSCoreHealthSet::PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data)
+{
+	Super::PostGameplayEffectExecute(Data);
+
+	if (Data.EvaluatedData.Attribute == GetHealthAttribute())
+	{
+		SetHealth(FMath::Clamp(GetHealth(), 0.f, GetMaxHealth()));
+	}else if (Data.EvaluatedData.Attribute == GetMaxHealthAttribute())
+	{
+		SetMaxHealth(FMath::Max(Data.EvaluatedData.Magnitude, 1.f));
+		SetHealth(FMath::Clamp(GetHealth(), 0.f, GetMaxHealth()));
+	}
+}
+
+void UFPSCoreHealthSet::OnRep_Health(const FGameplayAttributeData& OldHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UFPSCoreHealthSet,Health,OldHealth);
+}
+
+void UFPSCoreHealthSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UFPSCoreHealthSet,MaxHealth,OldMaxHealth);
+}
---
+++ Source/FPSCore/GAS/Attributes/FPSCoreHealthSet.h
@@ -0,0 +1,73 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "AttributeSet.h"
+#include "FPSCoreHealthSet.generated.h"
+
+#define FPSCORE_ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+UCLASS()
+class FPSCORE_API UFPSCoreHealthSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+public:
+	UFPSCoreHealthSet();
+	
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+	
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+	virtual void PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue) override;
+	virtual void PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data) override;
+	
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing= OnRep_Health, Category="FPS Core|Health")
+	FGameplayAttributeData Health;
+	
+	FPSCORE_ATTRIBUTE_ACCESSORS(UFPSCoreHealthSet, Health);
+	
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing= OnRep_MaxHealth, Category="FPS Core|Health")
+	FGameplayAttributeData MaxHealth;
+	
+	FPSCORE_ATTRIBUTE_ACCESSORS(UFPSCoreHealthSet, MaxHealth);
+	
+	UFUNCTION(BlueprintPure,Category="FPS Core|Health")
+	float GetHealthValue() const
+	{
+		return GetHealth();
+	}
+	
+	UFUNCTION(BlueprintPure,Category="FPS Core|Health")
+	float GetMaxHealthValue() const
+	{
+		return GetMaxHealth();
+	}
+	
+	UFUNCTION(BlueprintPure,Category="FPS Core|Health")
+	float GetHealthNormalized() const
+	{
+		const float CurrentMaxHealth = GetMaxHealth();
+		return CurrentMaxHealth > 0.0f ? GetHealth() / CurrentMaxHealth : 0.0f;
+	}
+	
+	UFUNCTION(BlueprintPure,Category="FPS Core|Health")
+	bool IsOutOfHealth() const
+	{
+		return GetHealth() <= 0.0f;
+	}
+	
+protected:
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth);
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth);
+	
+#undef FPSCORE_ATTRIBUTE_ACCESSORS
+
+};
